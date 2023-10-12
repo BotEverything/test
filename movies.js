2790179e9a88428ac1774c6afca9a4d1f6e3f3a5
BotEverything <104335640+BotEverything@users.noreply.github.com>
2023-10-12 18:13:59 -0500

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,12 @@
+export const getSmashystreamUrl = (tmdbID) =>{
+    return `https://embed.smashystream.com/playere.php?tmdb=${tmdbID}`
+}
+export const getSuperembedUrl = (tmdbID) =>{
+    return `https://multiembed.mov/directstream.php?video_id=${tmdbID}&tmdb=1`
+}
+export const get2embedUrl = (tmdbID) =>{
+    return `https://www.2embed.cc/embed/${tmdbID}`
+}
+export const getvidsrcUrl = (tmdbID) =>{
+    return `https://vidsrc.to/embed/movie/${tmdbID}`
+}
